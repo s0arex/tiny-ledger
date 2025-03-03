854e9dafb3983839244b532cef8859556ebecc94
Carlos Carvalho <carlos.soares.de.carvalho@gmail.com>
2025-03-03 15:07:34 +0000

    add basic project structure

---
+++ src/main/java/io/fincorp/tiny_ledger/TinyLedgerApplication.java
@@ -0,0 +1,13 @@
+package io.fincorp.tiny_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/fincorp/tiny_ledger/TinyLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fincorp.tiny_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
